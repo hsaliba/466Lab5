7aaa1f5d4e080c9dd2688385eb02722f009ae0a1
skrapil <skrapil@calpoly.edu>
2012-05-13 23:28:18 -0700

    added adjusted weighted sum method. works....dunno how accurately

--- Filter.java
+++ Filter.java
@@ -4,6 +4,8 @@ import java.io.*;
 
 public class Filter {
 
+   //calculated with the getk function below
+   private static final double k = .000000022370102912772287;
    //note: elemement 0 = # of jokes rated NOT a rating
    private static ArrayList<ArrayList<Double>> data; 
 
@@ -55,6 +57,37 @@ public class Filter {
       return top / (Math.sqrt(bot1*bot2));
    } 
 
+   public static double getk() {
+      double sum = 0;
+
+      for (int i = 0; i < data.size(); i++) {
+         for (int j = i+1; j < data.size(); j++) {
+            sum += cosineSim(data.get(i), data.get(j));
+         }
+      }
+      return 1/sum;
+   }
+
+   //currently uses itself in the calculation....shouldn't matter though
+   public static double adjustedWeightedSum(ArrayList<Double> u, int ndx) {
+      double uavg = 0.0;
+      double ans = 0.0;
+      double sum = 0.0;
+
+      for (int i = 1; i < u.size(); i++)
+         if (Double.compare(u.get(i), 99.0) != 0) 
+            uavg += u.get(i);
+      uavg = uavg / (u.size()-1);
+
+      for (int i = 0; i < data.size(); i++) {
+         double val = data.get(i).get(ndx);
+         sum += cosineSim(u, data.get(i)) * ((Double.compare(val, 99.0) == 0 ? 0 : val) - uavg);
+      } 
+      ans = uavg + (k * sum);
+      
+      return ans;      
+   }
+
    public static void main(String[] args) {
 
       if (args.length != 1) {
@@ -64,9 +97,14 @@ public class Filter {
 
 
       parseCSV(args[0]);
-      for (int i = 0; i < 10; i++) 
+      /*for (int i = 0; i < 10; i++) 
          for (int j = i+1; j < 10; j++) 
             System.out.println(cosineSim(data.get(i), data.get(j)));
-
+*/
+      //some example tests
+      System.out.println(adjustedWeightedSum(data.get(11), 1));
+      System.out.println(adjustedWeightedSum(data.get(11), 2));
+      System.out.println(adjustedWeightedSum(data.get(11), 3));
+      System.out.println(adjustedWeightedSum(data.get(11), 4));
    }
 }
